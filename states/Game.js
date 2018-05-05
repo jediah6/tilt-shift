77d278c4b2e68af91a53e5e766ca954f864add61
Jediah Conachan <jediah6@cs.washington.edu>
2018-05-05 01:23:52 +0000

    testing

--- states/Game.js
+++ states/Game.js
@@ -17,6 +17,8 @@ Game.prototype = {
 		this.a = game.input.keyboard.addKey(Phaser.Keyboard.A);
 		this.s = game.input.keyboard.addKey(Phaser.Keyboard.S);
 		this.d = game.input.keyboard.addKey(Phaser.Keyboard.D);
+		this.e = game.input.keyboard.addKey(Phaser.Keyboard.E);
+		
 		this.cursors = game.input.keyboard.createCursorKeys();
 	},
 
@@ -39,8 +41,8 @@ Game.prototype = {
         game.physics.p2.enable(beam);
         beam.body.data.gravityScale = 0;
 
+        /*
         // ball creation 
-	    
 	    ball = game.add.sprite(game.world.centerX, game.world.centerY - 200, 'ball');
 	    ball.anchor.set(0.5);
 	    game.physics.p2.enable(ball);
@@ -54,7 +56,7 @@ Game.prototype = {
 
 	    // game.camera.setSize(window.screen.width, window.screen.height);
 	     game.camera.follow(ball);
-
+        */
 
         //game.camera.follow(beam);
 	},
@@ -76,7 +78,7 @@ Game.prototype = {
         // horizontal movement
         if (this.a.isDown) {
             beam.body.moveLeft(300);
-        } else if (this.d.isDown) {
+        } else if (this.e.isDown) {
             beam.body.moveRight(300);
         }
         
@@ -97,5 +99,45 @@ Game.prototype = {
             beam.body.rotateRight(10);
         }
         */
+	}
+	
+	render : function() {
+	        var x = 32;
+        var y = 0;
+        var yi = 32;
+    
+        game.debug.text('Viewport', x, y += yi);
+    
+        game.debug.text('Viewport Width: ' + game.scale.viewportWidth, x, y += yi);
+        game.debug.text('window.innerWidth: ' + window.innerWidth, x, y += yi);
+        game.debug.text('window.outerWidth: ' + window.outerWidth, x, y += yi);
+    
+        game.debug.text('Viewport Height: ' + game.scale.viewportHeight, x, y += yi);
+        game.debug.text('window.innerHeight: ' + window.innerHeight, x, y += yi);
+        game.debug.text('window.outerHeight: ' + window.outerHeight, x, y += yi);
+    
+        game.debug.text('Document', x, y += yi*2);
+    
+        game.debug.text('Document Width: ' + game.scale.documentWidth, x, y += yi);
+        game.debug.text('Document Height: ' + game.scale.documentHeight, x, y += yi);
+    
+        //  Device: How to get device size.
+    
+        //  Use window.screen.width for device width and window.screen.height for device height. 
+        //  .availWidth and .availHeight give you the device size minus UI taskbars. (Try on an iPhone.) 
+        //  Device size is static and does not change when the page is resized or rotated.
+    
+        x = 350;
+        y = 0;
+    
+        game.debug.text('Device', x, y += yi);
+    
+        game.debug.text('window.screen.width: ' + window.screen.width, x, y += yi);
+        game.debug.text('window.screen.availWidth: ' + window.screen.availWidth, x, y += yi);
+        game.debug.text('window.screen.height: ' + window.screen.height, x, y += yi);
+        game.debug.text('window.screen.availHeight: ' + window.screen.availHeight, x, y += yi);
+    
+        game.debug.text('beam angle: ' + beam.body.rotation, x, y += yi);
+        game.debug.text('pointer angle: ' + angle, x, y += yi);
 	}
 }
